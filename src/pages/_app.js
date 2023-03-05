6126a6f306287bf9e6ac09bfac8e7f9304b7973b
milad <mohammadreza.khorrami21@gmail.com>
2023-03-05 09:28:51 +0330

    google tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -114,6 +114,15 @@ export default function App({ Component, pageProps }) {
   return (
     <>
         <div style={{ height: "0px" }}>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+      {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM-M7DBDR7}');
+      `}
+      </Script>
         <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-YN69BQP0B0"/>
           <Script
           id='google-analytics'
