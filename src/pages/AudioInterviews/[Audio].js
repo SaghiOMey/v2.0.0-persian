f0e76f1a7f0975abd465e85736e7f856bd9e91b6
milad <mohammadreza.khorrami21@gmail.com>
2023-04-29 20:08:13 +0330

    edited App

--- src/pages/AudioInterviews/[Audio].js
+++ src/pages/AudioInterviews/[Audio].js
@@ -683,7 +683,7 @@ function settimeout(){
             <br />
             <br />
             <div className="mx-auto text-center max-w-2xl py-16 px-4 sm:py-4 sm:px-6 lg:max-w-7xl lg:px-8">
-            <div className="mt-6 grid grid-cols-1 gap-y-4 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-4">
+            {/* <div className="mt-6 grid grid-cols-1 gap-y-4 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-4">
                 <>
                   {messages.Message ? messages.Message.map((review) => (
                     <>
@@ -753,7 +753,7 @@ function settimeout(){
                     </div>
                   )}
                 </>
-            </div>
+            </div> */}
           </div>
             <br />
             <br />
