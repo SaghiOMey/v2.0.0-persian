08cee0a7dd0bbfb6c30710d0f109430a4250dfa4
milad <mohammadreza.khorrami21@gmail.com>
2023-04-30 09:03:40 +0330

    edited

--- src/pages/AudioInterviews/[Audio].js
+++ src/pages/AudioInterviews/[Audio].js
@@ -74,12 +74,12 @@ export default function Persian(props) {
 
   const [form, setForm] = useState({
     rating: 0,
-    // name: props.user.displayName,
+    name: props.user.displayName,
     message: "",
     status: 0,
     Date: Date().slice(4,10)+','+Date().slice(10,15),
-    // nameep: result.name,
-    // ep: result.href
+    nameep: result.name,
+    ep: result.href
   });
   const re = comments ? comments.Comments.find((comment) => comment.name === result.href) : false
   // const me = comments ? comments.Comments.find((comment) => comment.ep === result.href) : false
@@ -220,7 +220,7 @@ function settimeout(){
   },3000)
 }
   // const show = props.episodes.slice(-4).reverse().find((episode) => episode.href === router.asPath.replace("/AudioInterviews/", ""))
-  console.log(props.user.displayName);
+  // console.log(props.user.displayName);
   return (
     <>
      <Index />
@@ -604,7 +604,7 @@ function settimeout(){
                                           )}
                                           aria-hidden="true"
                                           name="rating"
-                                          onClick={() => setForm({rating: rating + 1, name: form.name, nameep: result.name, message: form.message, status: 0, Date: Date().slice(4,10)+','+Date().slice(10,15), ep: form.ep})}
+                                          // onClick={() => setForm({rating: rating + 1, name: form.name, nameep: result.name, message: form.message, status: 0, Date: Date().slice(4,10)+','+Date().slice(10,15), ep: form.ep})}
                                         />
                                       ))}
                                     </div>
--- src/pages/[Episode].js
+++ src/pages/[Episode].js
@@ -72,12 +72,12 @@ export default function Episode(props) {
   );
   const [form, setForm] = useState({
     rating: 0,
-    // name: props.user.displayName,
+    name: props.user.displayName,
     message: "",
     status: 0,
     Date: Date().slice(4,10)+','+Date().slice(10,15),
-    // nameep: result.name,
-    ep: result.href ? result.href : ""
+    nameep: result.name,
+    ep: result.href
   });
   const re = comments ? comments.Comments.find((comment) => comment.name === result.href) : false
   // const me = comments ? comments.Comments.find((comment) => comment.ep === result.href) : false
