595295ce88cf9ae9d4db66a0db758c7c156becec
milad <mohammadreza.khorrami21@gmail.com>
2023-05-10 15:24:26 +0330

    edited

--- src/pages/index.js
+++ src/pages/index.js
@@ -306,7 +306,7 @@ export default function index(props) {
                       leaveFrom="transform opacity-100 scale-100"
                       leaveTo="transform opacity-0 scale-95"
                     >
-                      <Menu.Items className="absolute min-w-max min-h-max right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-neutral-800 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Items className="absolute -mr-16 md:mr-0 w-60 md:min-w-max min-h-max right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-neutral-800 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                         <Menu.Item>
                           {({ active }) => (
                             // eslint-disable-next-line jsx-a11y/anchor-is-valid
