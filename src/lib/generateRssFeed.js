de397db7526fd9d72ce8b90dff52c190d6d070b6
milad <mohammadreza.khorrami21@gmail.com>
2023-05-13 10:13:38 +0330

    edited

--- src/lib/generateRssFeed.js
+++ src/lib/generateRssFeed.js
@@ -18,8 +18,8 @@ export default async function getRSS() {
   allBlogs.map((post) => {
     feed.item({
       title: post.title,
-      url: `${siteURL}/blogs/${post.id}`,
-      // url: post.url,
+      // url: `${siteURL}/blogs/${post.slug}`,
+      url: post.url,
       date: post.date,
       description: post.describtion,
     });
