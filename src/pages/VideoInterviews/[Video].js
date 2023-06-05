dba0a5e7a1649d66816c5419d9797f1cb9dbafd9
milad <mohammadreza.khorrami21@gmail.com>
2023-06-05 15:23:32 +0330

    edited

--- src/pages/VideoInterviews/[Video].js
+++ src/pages/VideoInterviews/[Video].js
@@ -182,6 +182,8 @@ export default function Video(props) {
             </div>
             <br />
             <br />
+            <br />
+            <br />
             <div className="text-center text-white font-bold text-xs md:text-base mt-3">
               <svg class="w-6 h-6 mx-auto text-yellow-500 animate-bounce" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 13l-7 7-7-7m14-8l-7 7-7-7" />
