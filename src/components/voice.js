758748895f3acecad08be0bcf4b73d72732a88b9
milad <mohammadreza.khorrami21@gmail.com>
2023-07-18 12:34:48 +0330

    edited

--- src/components/voice.js
+++ src/components/voice.js
@@ -8,6 +8,7 @@ import { AudioRecorder, useAudioRecorder } from "react-audio-voice-recorder";
 import { getStorage, ref, uploadBytes } from "firebase/storage";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
+import Link from "next/link";
 
 
 //firebase config
@@ -25,6 +26,8 @@ firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
+
+const signIn = () => auth.signInWithPopup(provider);
 
 export default function Voice() {
   const form1 = useRef();
@@ -153,7 +156,7 @@ export default function Voice() {
           >
             <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
 
-            <div className="fixed inset-0 z-10 overflow-y-auto">
+            {/* <div className="fixed inset-0 z-10 overflow-y-auto">
               <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                 <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                   <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
@@ -184,7 +187,45 @@ export default function Voice() {
                   </div>
                 </div>
               </div>
-            </div>
+            </div> */}
+            <div className="fixed inset-0 z-10 overflow-y-auto">
+                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                      <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                        <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+                          <div className="text-center">
+                            <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+                              Please First Signin
+                            </h1>
+                            <p className="mt-6 text-base leading-7 text-gray-600">
+                              Sorry, To send Voice Please
+                              Signin.
+                            </p>
+                            <div className="mt-10 flex items-center justify-center gap-x-6">
+                              <button
+                                onClick={signIn}
+                                className="rounded-md bg-yellow-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-yellow-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-500"
+                              >
+                                Go to Signin
+                              </button>
+                              <Link
+                                href="/Contact"
+                                className="text-sm font-semibold text-gray-900"
+                              >
+                                Contact support{" "}
+                                <span aria-hidden="true">&rarr;</span>
+                              </Link>
+                            </div>
+                          </div>
+                        </main>
+                        <button type="button" onClick={() => setOpen(false)} class="absolute right-4 top-4 text-gray-400 hover:text-gray-500 sm:right-6 sm:top-8 md:right-6 md:top-6 lg:right-8 lg:top-8">
+            <span class="sr-only">Close</span>
+            <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
+                      </div>
+                    </div>
+                  </div>
             </div>
             }
           </div>
