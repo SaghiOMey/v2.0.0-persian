1edafeb347e89db7e3d3e1a84f568c839f1c7541
milad <mohammadreza.khorrami21@gmail.com>
2023-07-23 09:58:13 +0330

    edited

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -23,8 +23,7 @@ const firebaseConfig = {
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
       body: payload.notification.body,
-      icon: payload.notification.image,
-      link: "https://saghiomey.netlify.app/Dr.Sonia-Dadseresht(Last-persian-episode)"
+      icon: payload.notification.image
     };
   
     self.registration.showNotification(notificationTitle,
